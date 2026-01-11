f107e0ddef61a436dc56c0c4f71c758a367088ae
PR-Tom <nguyentienhuy3008@gmail.com>
2026-01-11 14:49:45 +0700

    Add files via upload

---
+++ assignment 1/bt4.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+sum_numbers = a + b + c
+product = a * b * c
+average = sum_numbers / 3
+
+print("Sum:", sum_numbers)
+print("Product:", product)
+print("Average:", average)
